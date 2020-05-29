83238ac5981db39e0d562ba65d2333d6bf364e1b
Dazpel <alexxdav97@gmail.com>
2020-05-29 12:13:53 -0400

    end trip

--- src/Components/groupImage/GroupImage.jsx
+++ src/Components/groupImage/GroupImage.jsx
@@ -31,8 +31,6 @@ export default class GroupImage extends Component {
 
   // this method handles just the file upload
   handleFileUpload = async (e) => {
-    console.log('The file to be uploaded is: ', e.target.files[0]);
-
     const uploadData = new FormData();
     // imageUrl => this name has to be the same as in the model since we pass
     // req.body to .create() method when creating a new thing in '/api/things/create' POST route
@@ -43,7 +41,6 @@ export default class GroupImage extends Component {
       .then((response) => {
         // console.log('response is: ', response);
         // after the console.log we can see that response carries 'secure_url' which we can use to update the state
-        console.log(response);
         this.setState({ imageUrl: response.data.secure_url, saved: false });
       })
       .catch((err) => {
@@ -61,7 +58,7 @@ export default class GroupImage extends Component {
       saved: true,
     });
 
-    this.updateArr(this.props.currentUser);
+    this.updateArr(this.props.currentUser, this.props.query);
   };
 
   render() {
@@ -77,7 +74,7 @@ export default class GroupImage extends Component {
     } else {
       currentTrip = this.props.currentUser.currentTrip;
     }
-
+    
     return (
       <div>
         <div className="folderContainer">
